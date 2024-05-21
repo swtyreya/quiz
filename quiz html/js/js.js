102d96c5d3ad033e35e3debe20e4dc4dd86baba6
swtyreya <espiritureya@gmail.com>
2024-05-21 17:12:55 +0800

    Add files via upload

---
+++ quiz html/js/js.js
@@ -0,0 +1,14 @@
+function input1(){
+    document.getElementById("label1").style.color="red";
+    document.getElementById("label3").style.color="green";
+    
+}
+function input2(){
+    document.getElementById("label2").style.color="red";
+    document.getElementById("label3").style.color="green";
+}
+function input3(){
+    document.getElementById("label3").style.color="green";
+}
+
+
